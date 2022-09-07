c06980a5d338614ad0d301cd4522f5fdc0591c5a
Ahmed Ibrahim <qwertu12467898@gmail.com>
2022-09-07 21:21:57 +0200

    Package for mathimatical operations

---
+++ Build Packages/packageTask/src/file_for_math.py
@@ -0,0 +1,15 @@
+class class_for_math:
+    def add(x,y):
+        print('The Add of Two Number is ',x+y)
+
+    def sub(x,y):
+        print('The sub of Two Number is ',x-y)
+
+    def multi(x,y):
+        print('The multi of Two Number is ',x*y)
+
+    def divide(x,y):
+        print('The divide of Two Number is ',x/y)
+
+    def remender(x,y):
+        print('The remender of Two Number is ',x%y)
