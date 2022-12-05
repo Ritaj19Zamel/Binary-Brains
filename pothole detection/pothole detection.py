05dce59b69ae124a7e93ff0e6038bcf9a142057e
Zekoo6666 <zakaria.ahmed6765@gmail.com>
2022-12-05 22:21:28 +0200

    update become realtime

--- pothole detection/pothole detection.py
+++ pothole detection/pothole detection.py
@@ -2,13 +2,15 @@ import numpy
 import cv2
 
 image_number=1
-
-while(image_number < 4):
-    img = cv2.imread(('C:\\Users\\Zakaria Ahmed\\Downloads\\Graduation Project\\Pothole-Detection\\pothole_')+str(image_number)+'.jpeg')#image1
+cap = cv2.VideoCapture(0)
+while True :
+    # img = cv2.imread(('C:\\Users\\Zakaria Ahmed\\Downloads\\Graduation Project\\Pothole-Detection\\pothole_')+str(image_number)+'.jpeg')#image1
     # cv2.imshow('image',img)
-    blur = cv2.blur(img,(5,5))
-    gblur = cv2.GaussianBlur(img,(5,5),0)
-    median = cv2.medianBlur(img,5)
+    ret, frame = cap.read()
+    # cv2.imshow('frame',frame)
+    blur = cv2.blur(frame,(5,5))
+    gblur = cv2.GaussianBlur(frame,(5,5),0)
+    median = cv2.medianBlur(frame,5)
     kernel = numpy.ones((5,5),numpy.uint8)
     erosion = cv2.erode(median,kernel,iterations = 1)
     dilation = cv2.dilate(erosion,kernel,iterations = 5)
@@ -19,7 +21,9 @@ while(image_number < 4):
     ret,threshold=cv2.threshold(edges.copy(),0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
     contours,_=cv2.findContours(threshold,cv2.RETR_LIST,cv2.CHAIN_APPROX_NONE)
 
-    cv2.drawContours(img,contours,-1,(0,0,255),2)
-    cv2.imshow("Show",img)
+    cv2.drawContours(frame,contours,-1,(0,0,255),2)
+    cv2.imshow("Show",frame)
+    if cv2.waitKey(1) == ord('q'):
+            break
     image_number +=1
-    cv2.waitKey()#to hold image
+    cv2.waitKey(1)#to hold image
